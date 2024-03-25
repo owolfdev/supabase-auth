c012794200556f135dadc0fc4d1e426154030b50
owolfdev <owolfdev@gmail.com>
2024-03-26 06:08:38 +0700

    update protected

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -12,6 +12,12 @@ export default async function ProtectedPage() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", user?.id)
+    .single();
+
   if (!user) {
     return redirect("/login");
   }
@@ -23,6 +29,7 @@ export default async function ProtectedPage() {
           This is a protected page that you can only see as an authenticated
           user
         </div>
+
         {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
             <DeployButton />
@@ -32,6 +39,10 @@ export default async function ProtectedPage() {
       </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        <div>Hey, {user.email}!</div>
+        <div>User id: {user.id}</div>
+        <div>Name: {profile.name || "no name assigned"}</div>
+        {/* <div>Profile name: {JSON.stringify(profile)}</div> */}
         {/* <Header /> */}
         {/* <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
