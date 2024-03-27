39ced746413195c352c1384018091d2ecc179b55
owolfdev <owolfdev@gmail.com>
2024-03-27 10:59:54 +0700

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Index() {
   const supabase = createClient();
@@ -22,12 +23,18 @@ export default async function Index() {
   if (!user) {
     return redirect("/login");
   }
+
   return (
     <div className="flex-1 flex flex-col justify-center w-full px-8 sm:max-w-lg gap-2 py-24">
       <h1 className="font-bold text-4xl pb-4 text-center">
         Supabase Auth Template
       </h1>
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">Hi</div>
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        Hi {profile?.name || user?.email}
+        <div>
+          <Link href="/profile">Check out your profile page.</Link>
+        </div>
+      </div>
     </div>
   );
 }
